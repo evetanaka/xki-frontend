e8b6b58fe36bec4d4eaf955d5d5815011aa31adc
Eve Tanaka <eve@gmstn.vc>
2026-04-15 23:32:36 +0000

    fix: countdown banner sticky below nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import CountdownBanner from './components/CountdownBanner';
 export default function App({ navOnly }: { navOnly?: boolean }) {
   return (
     <div className="bg-[#050505] text-[#E0E0E0] font-sans antialiased selection:bg-white selection:text-black overflow-x-hidden min-h-screen flex flex-col">
-      <CountdownBanner />
       <Nav />
+      <CountdownBanner />
       <Outlet />
       {!navOnly && <Footer />}
     </div>
